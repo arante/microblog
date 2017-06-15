f73990e110effc54c75de5846c407a5c813519cf
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-06-16 01:01:41 +0800

    microblog: Templates to the rescue

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    new_user = {'nickname': 'Billy'}  # Mock data
+
+    return render_template('index.html', title='Home', user=new_user)
