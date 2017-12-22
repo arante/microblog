3887eee39fc9f4205aba4a486bf5c4103906ab32
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-22 23:55:12 +0800

    feat: Created a test view for index

--- app/views.py
+++ app/views.py
@@ -3,4 +3,25 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {
+        'nickname': 'Billy Wilson'
+    }
+
+    return '''
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+		<title>Billy Wilson Arante | Home</title>
+
+            </head>
+            <body>
+
+		<h1>Hello, ''' + user['nickname'] + '''</h1>
+
+            </body>
+        </html>
+    '''
