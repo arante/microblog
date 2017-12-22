245593ff0863381939a6666c0aaf4eca1a13ddca
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-23 00:11:05 +0800

    feat: Templates to the rescue

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
@@ -7,21 +8,4 @@ def index():
         'nickname': 'Billy Wilson'
     }
 
-    return '''
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-
-		<title>Billy Wilson Arante | Home</title>
-
-            </head>
-            <body>
-
-		<h1>Hello, ''' + user['nickname'] + '''</h1>
-
-            </body>
-        </html>
-    '''
+    return render_template('index.html', title='Home', user=user)
