b702d84debdee36007247875a1289bf0ce133160
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-26 21:39:10 +0800

    feat: Part 5 - Revisiting our User model (incomplete)

--- app/models.py
+++ app/models.py
@@ -6,6 +6,28 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+
+    @property
+    def is_active(self):
+        return True
+
+
+    @property
+    def is_anonymous(self):
+        return False
+
+
+    def get_id(self):
+        try:
+	    return unicode(self.id)  # Python 2
+	except NameError:
+	    return str(self.id)  # Python 3
+
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
@@ -15,6 +37,7 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
